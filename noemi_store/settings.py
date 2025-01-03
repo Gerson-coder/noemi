1d3713d1c73bafcda807c772fadf67280cb02a9a
Astro dev <96029847+Gerson-coder@users.noreply.github.com>
2025-01-03 15:06:09 -0500

    Update settings.py

--- noemi_store/settings.py
+++ noemi_store/settings.py
@@ -124,8 +124,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = 'static'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATIC_ROOT = os.path.join(BASE_DIR,'staic')
+
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
