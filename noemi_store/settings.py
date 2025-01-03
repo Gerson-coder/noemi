3996207429ff2832b26891c9afbea55bf9502bcd
Astro dev <96029847+Gerson-coder@users.noreply.github.com>
2025-01-03 15:01:26 -0500

    Update settings.py

--- noemi_store/settings.py
+++ noemi_store/settings.py
@@ -20,11 +20,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config('SECRET_KEY')
+SECRET_KEY = 'django-insecure-$g%bu(clatbf(_mp@ab-i6((-*dhb9&dmi4u$5t^lsri5&i28_'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = config('DEBUG' ,cast=bool )
+DEBUG = True
 
 
 ALLOWED_HOSTS = []
